3a1df3f4ef86628fcee0a95b8d9a3ee41f83f495
Mimi <miraclef60@gmail.com>
2021-11-29 14:02:31 +0100

    Refactored index page of the develooper dasboard

--- pages/index.js
+++ pages/index.js
@@ -5,58 +5,56 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head></Head>
+      <Head>
+        <title>FlintAPI | Developer</title>
+        <meta name="description" content="FlintAPI Developer Dashboard" />
+        <link rel="icon" href="/ico.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0,  user-scalable=no, maximum-scale=1"
+        />
+      </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://flintapi.io">FLINT API</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          For a good experience make sure you are not accessing this developer
+          dashboard on a mobile device or the likes{" "}
+          {/* <code className={styles.code}>pages/index.js</code> */}
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <a href="https://dev.flintapi.io/login" className={styles.card}>
+            <h2>Login &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Already got an account with us? proceed to login and start
+              building on already available API suit.
+            </p>
+          </a>
+
+          <a href="https://dev.flintapi.io/signup" className={styles.card}>
+            <h2>Sign Up &rarr;</h2>
+            <p>
+              Create an account lets get the party started, use our available
+              API suit to build your next NFT project.
             </p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://flintapi.io" target="_blank" rel="noopener noreferrer">
           Powered by{" "}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image
+              src="/logo-black.png"
+              alt="Flint API Logo"
+              width={82}
+              height={21}
+            />
           </span>
         </a>
       </footer>
